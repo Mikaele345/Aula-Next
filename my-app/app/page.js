62c16cb440f24385b291212188eb24aed2c97f1d
Mikaele Ferreira <mikaele.santos@academico.ifpb.edu.br>
2025-11-25 10:40:10 -0300

    Novas Atualizações

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -6,6 +6,15 @@ export default function Home() {
       <h1>
         Bem-vindo á Biblioteca do IFPB!
       </h1>
+      <Image
+      src="/ifpb-logo.png"
+      alt="IFPB logo"
+      width={200}
+      height={300}
+      />
+      <p>
+        Explore nossa coleção de livros, artigos e recursos.
+      </p>
     
     </div>
   );
