0c966788dd388c086c51afd7189844d04fac2812
Mikaele dos Santos Ferreira <202312060032@ad.ifpb.edu.br>
2025-12-02 10:30:41 -0300

    Atualizou hoje

--- my-app/app/components/header/Header.js
+++ my-app/app/components/header/Header.js
@@ -6,22 +6,22 @@ export default function Header(){
     return(
         <header className={styles.header}>
             <h1>
-                <Link href="/" className={styles.Link}>
+                <Link href="/" className={styles.link}>
                 IFPB-Biblioteca
                 </Link>
-                </h1>
+            </h1>
 
-                <nav>
-                    <Link href="/" className={styles.link}>
-                    Home
-                    </Link>
-                    <Link href="/cadastro" className={styles.link}>
-                    Cadastro de Livros
-                    </Link>
-                    <Link href="/sobre" className={styles.link}>
-                    Sobre
-                    </Link>
-                </nav>
+            <nav>
+                <Link href="/" className={styles.link}>
+                Home
+                </Link>
+                <Link href="/cadastro" className={styles.link}>
+                Cadastro de Livros
+                </Link>
+                <Link href="/sobre" className={styles.link}>
+                Sobre
+                </Link>
+            </nav>
         </header>
 
 
